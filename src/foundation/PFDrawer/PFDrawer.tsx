02175b3992054427182cb83ae6a0f6472a5c4344
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2022-07-07 14:16:22 -0500

    menu

--- src/foundation/PFDrawer/PFDrawer.tsx
+++ src/foundation/PFDrawer/PFDrawer.tsx
@@ -1,6 +1,12 @@
 import React from "react"
 
-import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Icon, useDisclosure } from "@chakra-ui/react"
+import { Box, Button, Divider, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Icon, useDisclosure } from "@chakra-ui/react"
+import { FaInstagram, FaSpotify } from "react-icons/fa"
+import { SiTiktok } from "react-icons/si"
+import PFButton from "../PFButton/PFButton"
+import { PFLink } from "../PFLink/PFLink"
+import PFParagraph from "../PFParagraph/PFParagraph"
+import PFHeading from "../PFHeading/PFHeading"
 
 type PFDrawerProps = {
   drawerSize: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'full',
@@ -21,30 +27,48 @@ export default function PFDrawer({ drawerSize }: PFDrawerProps) {
   return (
     <>
 
-      <Button
+      <Button position="relative"
         backgroundColor={"fondo"}
         onClick={() => handleClick(drawerSize)}
         key={drawerSize}
-        m={0} p={0}
-      >      <svg width="50" height="25" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        width="50px" height="40px"
+      >      <svg width="25" height="25" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M4 6H20M4 12H20M13 18H20" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
         </svg></Button>
 
-      <Drawer onClose={onClose} isOpen={isOpen} size={drawerSize}>
+      <Drawer onClose={onClose} isOpen={isOpen} size={drawerSize} >
         <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>{`${drawerSize} drawer contents`}</DrawerHeader>
+        <DrawerContent backgroundColor="black">
+          <Box bg={"black"} py={{ xs: "18px", sm: "18px", md: "36px" }} px={{ xs: "12px", sm: "16px", md: "32px", lg: "64px", xl: "104px" }}>
+            <Box display={{ sm: "flex", md: "flex", lg: "none" }} >
+              <Box w={"100%"} display="flex" justifyContent={"space-between"} alignItems="center">
+                <PFHeading variant="h4" color="blanco" text="Kat Rogers Music" />
+
+                <DrawerCloseButton position={"inherit"} alignContent="center" color={"white"} width="50px" height="40px" />
+
+              </Box>
+            </Box>
+
+          </Box >
+          <Box mt="40px" p="24px" display={"flex"} flexDir="column" justifyContent={"start"} alignItems="start">
+
+            <PFHeading variant="h2" text="Home" color="white" />
+            <PFHeading variant="h2" text="DJ" color="white" />
+            <PFHeading variant="h2" text="Camera Work" color="white" />
+            <PFHeading variant="h2" text="Gallery" color="white" />
+          </Box>
+          <Divider></Divider>
           <DrawerBody>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Consequat nisl vel pretium lectus quam id. Semper quis lectus
-              nulla at volutpat diam ut venenatis. Dolor morbi non arcu risus
-              quis varius quam quisque. Massa ultricies mi quis hendrerit dolor
-              magna eget est lorem. Erat imperdiet sed euismod nisi porta.
-              Lectus vestibulum mattis ullamcorper velit.
-            </p>
+            <Box mt="40px" d="flex" gap="40px" justifyContent={"end"}>
+              <PFLink href='https://www.instagram.com/katrogersmusic/' isExternal><FaInstagram size="30px" /></PFLink>
+              <PFLink href='#spotify'><FaSpotify size="30px" /> </PFLink>
+              {/* <PFLink  href='#' isExternal><FaYoutube size="30px" /></PFLink> */}
+              <PFLink href='https://www.tiktok.com/@katrogersmusic?lang=en' isExternal><SiTiktok size="30px" /></PFLink>
+            </Box>
+            <Box >
+              {/* <PFButton variant="solid" size="size5" hover={{ bg: "linear-gradient(270deg, #1bd760 10.81%, #59EBFF 100%)" }}
+              ><PFParagraph variant="boton" text="Check out my Music!" /> </PFButton> */}
+            </Box>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
